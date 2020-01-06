9bc2f691bad79fa97c7cdaaa8f8b8061df36c77f
bab-git <bbkhosseini@gmail.com>
2020-01-06 17:20:41 +0100

    update

--- read_wave.py
+++ read_wave.py
@@ -11,7 +11,7 @@ import os
 os.chdir('/home/bhossein/BMBF project/code_repo')
 
 import matplotlib.pyplot as plt
-import scipy
+#import scipy
 
 from my_data_classes import create_datasets, create_loaders, read_data, create_datasets_file, smooth, wave_harsh_peaks, wave_harsh_peaks_all
 import my_data_classes
@@ -134,7 +134,7 @@ assert 1== 61090
 i_file = np.random.randint(8000, size = 1).item()
 #i_file = 7850
 
-i_class = 0 #0:normal  1:atrial
+i_class = 1 #0:normal  1:atrial
 #i_class = np.random.randint(2, size = 1)+1    
     
 #path = '/data/BMBF/sample/filtered atrial waves/8aae6985-c0b9-41d4-ac89-9f721b8019d2.wav'
